f1f312ac85a30806ab8fb0ee4aee89eb2d48f7bc
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-01-04 13:38:26 +0100

    imported AppHeader + styles.css deleted

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <h1>HEADER GOES HERE</h1>
+</template>
+
+<style lang="scss" scoped>
+//SCSS RELATIVO AL SOLO COMPONENTE
+</style>
